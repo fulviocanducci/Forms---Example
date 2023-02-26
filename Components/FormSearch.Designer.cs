ad648211790c8cf3c4d1d019e915c65ce9b3cb8b
Fulvio Cezar Canducci Dias <fulviocanducci@hotmail.com>
2023-02-26 12:24:29 -0300

    Add project files.

---
+++ Components/FormSearch.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Components
+{
+   partial class FormSearch
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         DataGridViewGeneral = new DataGridView();
+         ((System.ComponentModel.ISupportInitialize)DataGridViewGeneral).BeginInit();
+         SuspendLayout();
+         // 
+         // DataGridViewGeneral
+         // 
+         DataGridViewGeneral.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+         DataGridViewGeneral.Location = new Point(12, 12);
+         DataGridViewGeneral.Name = "DataGridViewGeneral";
+         DataGridViewGeneral.RowTemplate.Height = 25;
+         DataGridViewGeneral.Size = new Size(776, 426);
+         DataGridViewGeneral.TabIndex = 0;
+         DataGridViewGeneral.CellDoubleClick += DataGridViewGeneral_CellDoubleClick;
+         DataGridViewGeneral.PreviewKeyDown += DataGridViewGeneral_PreviewKeyDown;
+         // 
+         // FormSearch
+         // 
+         AutoScaleDimensions = new SizeF(7F, 15F);
+         AutoScaleMode = AutoScaleMode.Font;
+         ClientSize = new Size(800, 450);
+         Controls.Add(DataGridViewGeneral);
+         Name = "FormSearch";
+         Text = "FormSearch";
+         Load += FormSearch_Load;
+         KeyUp += FormSearch_KeyUp;
+         ((System.ComponentModel.ISupportInitialize)DataGridViewGeneral).EndInit();
+         ResumeLayout(false);
+      }
+
+      #endregion
+
+      private DataGridView DataGridViewGeneral;
+   }
+}
---
+++ Components/FormSearch.cs
@@ -0,0 +1,70 @@
+﻿namespace Components
+{
+   public partial class FormSearch : Form
+   {
+      public object? Item { get; private set; }
+
+      public FormSearch()
+      {
+         InitializeComponent();
+      }
+
+      public void SetDataSource(object data)
+      {
+         DataGridViewGeneral.DataSource = data;
+      }
+
+      public void SetColumns(params DataGridViewColumn[] columns)
+      {
+         DataGridViewGeneral.AutoGenerateColumns = false;
+         DataGridViewGeneral.AllowUserToAddRows = false;
+         DataGridViewGeneral.AllowUserToDeleteRows = false;
+         DataGridViewGeneral.AllowUserToResizeRows = false;
+         DataGridViewGeneral.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+         DataGridViewGeneral.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+         DataGridViewGeneral.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+         DataGridViewGeneral.Columns.Clear();
+         DataGridViewGeneral.Columns.AddRange(columns);
+      }
+
+      public void SetTitle(string title)
+      {
+         Text = title;
+      }
+
+      private void FormSearch_Load(object sender, EventArgs e)
+      {
+
+      }
+
+      private void DataGridViewGeneral_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+      {
+         if (sender != null && e != null && e.RowIndex > -1 && DataGridViewGeneral.CurrentRow != null && DataGridViewGeneral.Rows.Count > 0)
+         {
+            Item = DataGridViewGeneral.Rows[e.RowIndex].Cells[0].Value;
+            Close();
+         }
+      }
+
+      private void DataGridViewGeneral_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+      {
+         if (e.KeyCode == Keys.Enter)
+         {
+            if (sender != null && e != null && DataGridViewGeneral.CurrentRow != null && DataGridViewGeneral.Rows.Count > 0)
+            {
+               Item = DataGridViewGeneral.CurrentRow.Cells[0].Value;
+               Close();
+            }
+         }
+      }
+
+      private void FormSearch_KeyUp(object sender, KeyEventArgs e)
+      {
+         if (e.KeyCode == Keys.Escape)
+         {
+            Item = null;
+            Close();
+         }
+      }
+   }
+}
